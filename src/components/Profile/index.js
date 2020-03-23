fa1d0883d3a15d7ed1b424e1714c9c47490ad64a
Pazeto22 <gustavopazeto95@gmail.com>
2020-03-23 17:24:48 -0300

    create and link pages // use layout

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Gustavo Pazeto</h1>
+        <h2>Desenvolvedor Front-End Júnior</h2>
+        <p>
+            Estudante do curso de Análise e Desenvolvimento de Sistemas na FATEC-Franca.
+            </p>
+    </div>
+);
+
+export default Profile
